3baaae706304bf191538d55bdc2685cb0aeff4a0
Niukor <niukor@qq.com>
2021-03-02 10:27:22 +0800

    init tutools project

---
+++ src/main/java/com/tutools/tutools/TutoolsApplication.java
@@ -0,0 +1,13 @@
+package com.tutools.tutools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutools/tutools/TutoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutools.tutools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
